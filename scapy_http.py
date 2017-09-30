aa2a30de980aca5487b9e2b25f72adf1169025c4
qqiang2006 <344897531@qq.com>
2017-09-30 15:10:51 +0800

    icmp

--- scapy_http.py
+++ scapy_http.py
@@ -1,1 +1,7 @@
 #coding=utf8
+from scapy.all import *
+for i in range(2,255):
+    ip=IP(src='192.168.10.%d'%i,dst='192.168.10.194') 
+    tcp=TCP(sport=1024)
+    http="GET / HTTP/1.0\r\n"
+    print sr1(ip/tcp/http)
